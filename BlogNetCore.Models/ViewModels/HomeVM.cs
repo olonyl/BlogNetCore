be6b946be15e1b10d364b98bca7d5dba10cb17c0
olonyl <o.rocha@contolloconsulting.com>
2020-10-02 01:55:33 -0600

    Added Slider to Home Index

---
+++ BlogNetCore.Models/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogNetCore.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Slider> Sliders { get; set; }
+        public IEnumerable<Article> Articles{ get; set; }
+    }
+}
--- BlogNetCore/Areas/Client/Controllers/HomeController.cs
+++ BlogNetCore/Areas/Client/Controllers/HomeController.cs
@@ -6,33 +6,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BlogNetCore.Models;
+using Microsoft.EntityFrameworkCore.Metadata;
+using BlogNetCore.DataAccess.Data.Repository;
+using BlogNetCore.Models.ViewModels;
 
 namespace BlogNetCore.Controllers
 {
     [Area("Client")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            this.unitOfWork = unitOfWork;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Articles = unitOfWork.Article.GetAll(),
+                Sliders = unitOfWork.Slider.GetAll()
+            };
+            return View(homeVM);
         }
-
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Detail(int id)
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var article = unitOfWork.Article.GetFirstOrDefault(a=> a.Id == id);
+            if (article == null) return NotFound();
+            return View(article);
         }
     }
 }
